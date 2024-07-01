fe731108f52f0aea567445df07e0d13c46464378
Bruna Ester Rolim <brunaerrolim@gmail.com>
2024-07-01 19:46:55 -0300

    feat: adding compose ui theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation ("androidx.compose.foundation:foundation:1.6.8")
     implementation ("androidx.compose.material:material:1.6.8")
     implementation ("androidx.activity:activity-compose:1.9.0")
+    implementation ("androidx.compose.material3:material3:1.2.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     implementation ("androidx.compose.runtime:runtime-livedata:1.6.8")
 
--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -2,29 +2,21 @@ package com.example.reminderapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.reminderapp.ui.theme.ReminderAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+        setContent {
+            ReminderAppTheme {
 
-    @Composable
-        fun Greeting(name: String, modifier: Modifier = Modifier) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MaterialTheme {
-            Greeting(name = "Android")
+            }
         }
     }
 }
